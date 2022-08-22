9c68b6a5ba51ccf5785abc0bfda16ee65c6fea70
HACK-WU <2931580128@qq.com>
2022-08-22 18:46:38 +0800

    v1.0

--- LNMP_install.sh
+++ LNMP_install.sh
@@ -7,15 +7,67 @@
 #
 # 适用的软件版本：
 #	nginx:  nginx-1.22.0.tar.gz
-#   mysql:  mysql-boost-5.7.39.tar.gz(推荐) | mysql-5.7.39.tar.gz| mysql-5.7.39-linux-glibc2.12-x86_64
-#	php:
+#       mysql:  mysql-boost-5.7.39.tar.gz(推荐) | mysql-5.7.39.tar.gz| mysql-5.7.39-linux-glibc2.12-x86_64
+#	php:    php-7.4.30.tar.gz
 ########################################
-
 set -u
 #set -e
 
- yum install -y  openssl-devel  gcc gcc-c++		#安装编译工具
+NGINX_TAG=OFF					#是否开启安装功能，ON为开启，OFF为不开启
+MYSQL_TAG=OFF
+PHP_TAG=OFF
 
+
+NGINX_pkg=								#nginx的源码包包位置，绝对路径
+MYSQL_pkg=								#mysql的源码包位置，绝对路径
+PHP_pkg=							   #php源码包位置
+
+
+##############################   软件安装配置     #############################################
+NGINX_user=www							#运行软件的用户
+NGINX_basedir=/usr/local/nginx
+NGINX_configure="--prefix=$NGINX_basedir  --user=$NGINX_user --group=$NGINX_user --with-http_stub_status_module --with-http_ssl_module"
+
+MYSQL_user=mysql						#运行软件的用户
+MYSQL_basedir=/usr/local/mysql			#mysql的安装位置
+MYSQL_cmake=". \							
+-DCMAKE_INSTALL_PREFIX=$MYSQL_basedir \
+-DMYSQL_DATADIR=$MYSQL_basedir/data \
+-DMYSQ_TCP_PORT=3306 \
+-DMYSQL_UNIX_ADDR=$MYSQL_basedir/mysql.sock \
+-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+-DWITH_PARTITION_STORAGE_ENGINE=1 \
+-DWITH_FEDERATED_STORAGE_ENGINE=1 \
+-DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
+-DWITH_MYISAM_STORAGE_ENGINE=1 \
+-DENABLED_LOCAL_INFILE=1 \
+-DEXTRA_CHARSETS=all \
+-DDEFAULT_CHARSET=utf8mb4 \
+-DDEFAULT_COLLATION=utf8mb4_general_ci \
+-DWITH_SSL=system \
+-DWITH_BOOST=$MYSQL_basedir/boost \
+-DDOWNLOAD_BOOST=1
+"
+ 
+PHP_user=www						   #运行软件的用户
+PHP_basedir=/usr/local/php			   #软件安装位置
+PHP_configure="--prefix=$PHP_basedir --with-config-file-path=$PHP_basedir/etc/ \
+--with-libxml-dir  \
+--with-jpeg-dir  --with-png-dir  \
+--with-freetype-dir=/usr/local/freetype/  \
+--with-mysqli=$MYSQL_basedir/bin/mysql_config --with-iconv-dir \
+--enable-mbstring=all   --with-zlib  --with-libzip --with-curl \
+--enable-sockets   --enable-xml --enable-sysvsem   --enable-bcmath  \
+--with-pdo-mysql=$MYSQL_basedir   --with-fpm-group=$user  \
+--with-gd --without-pear    --with-gettext --enable-soap \
+--enable-fpm  --with-fpm-user=$user   --with-openssl --with-mhash \
+--enable-ftp --enable-maintainer-zts  --with-xmlrpc  --enable-pcntl \
+--enable-inline-optimization  --enable-shmop --enable-mbregex 
+"
+
+####################################################################################3
+ yum install -y epel-release.noarch
+ yum install -y  openssl-devel  gcc gcc-c++		#安装编译工具
 
 
 function tar_xf {    
@@ -49,17 +101,21 @@ function tar_xf {
 	echo "$pkgdir"				#返回解压后的目录名
 }
 
+
+PKG_DIR=null
 function install_pkg {
 #	参数：
 #		$1:		软件包的解压目录
-#		$2:		软件的编译配置		
+#		$*:		软件的编译配置		
 	local pkgdir=$1
-	local configure=$2
+	shift
+	local configure=$*
 	
+	PKG_DIR=$pkgdir
 	cd $pkgdir			
 	local prefix=$(echo $configure | cut  -d " "  -f 1 )
 	local prefix=${prefix#*--prefix=}
-	
+
 	if [ "$configure" != "null"  ];then
 		./configure $configure 
 		if [ ! "$?" -eq 0  ];then
@@ -85,10 +141,15 @@ function install_pkg {
 }
 
 ######################  NGINX ################################
-#yum -y install pcre-devel zlib-devel  #安装nginx依赖
-#pkg=/root/nginx-1.22.0.tar.gz
-#configure="--prefix=/usr/local/nginx  --user=www --group=www --with-http_stub_status_module --with-http_ssl_module"
-#install_pkg  $(tar_xf $pkg www )  $configure
+function nginx_main {
+	yum -y install pcre-devel zlib-devel  #安装nginx依赖
+	local pkg=$NGINX_pkg
+	local user=$NGINX_user
+	local configure=$NGINX_configure
+	install_pkg  $(tar_xf $pkg $user )  $configure
+}
+
+[ "$NGINX_TAG" == "ON"  ] && nginx_man
 
 #######################Mysql ###########################
 function init_mysql {    #mysql初始化
@@ -133,6 +194,7 @@ function install_mysql {
 #	参数：
 #		$1: 软件包的绝对路径
 #		$2: 运行软件的用户名
+#		$3: cmake配置
 yum -y install bison   ncurses-devel cmake libaio-devel 	#安装依赖
 
 dd if=/dev/zero of=/swapfile bs=1M count=2048       	#创建交换分区
@@ -153,23 +215,7 @@ fi
 
 cd $pkgdir
 
-cmake . \
--DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
--DMYSQL_DATADIR=/usr/local/mysql/data \
--DMYSQ_TCP_PORT=3306 \
--DMYSQL_UNIX_ADDR=/usr/local/mysql/mysql.sock \
--DWITH_INNOBASE_STORAGE_ENGINE=1 \
--DWITH_PARTITION_STORAGE_ENGINE=1 \
--DWITH_FEDERATED_STORAGE_ENGINE=1 \
--DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
--DWITH_MYISAM_STORAGE_ENGINE=1 \
--DENABLED_LOCAL_INFILE=1 \
--DEXTRA_CHARSETS=all \
--DDEFAULT_CHARSET=utf8mb4 \
--DDEFAULT_COLLATION=utf8mb4_general_ci \
--DWITH_SSL=system \
--DWITH_BOOST=/usr/local/mysql/boost \
--DDOWNLOAD_BOOST=1 
+cmake $3
 
 if [ "$?" -eq 0 ];then
 	install_pkg  $pkgdir null		#进入到软件包目录
@@ -184,11 +230,76 @@ fi
 
 }
 
-#pkg="/root/mysql-boost-5.7.39.tar.gz"
+function mysql_main {
+	local pkg=$MYSQL_pkg
+	local user=$MYSQL_user
+	local basedir=$MYSQL_basedir
+	local cmake=$MYSQL_cmake
+	install_mysql  $pkg $user $cmake	#编译安装软件包
+	init_mysql $basedir  $user			#初始化数据库
+}
 
-#install_mysql  $pkg mysql				#编译安装软件包
-#tar_xf $pkg mysql
-#init_mysql "/usr/local/mysql"  mysql	#初始化数据库
+[ "$MYSQL_TAG" == "ON"  ] && mysql_main
 
+################################# php ##################################
+ function init_php {
+#   参数
+#       $1:  安装路径
+#       $2:  解压后的软件包位置
 
+    local prefix=$1
+    local pkgdir=$2
+    cp $prefix/etc/php-fpm.conf.default $prefix/etc/php-fpm.conf
+    cp $prefix/etc/php-fpm.d/www.conf.default $prefix/etc/php-fpm.d/www.conf
+    cp $pkgdir/php.ini-development $prefix/etc/php.ini
+    ln -s $prefix/bin/* /usr/local/bin/
+    ln -s $prefix/sbin/* /usr/local/sbin/
+    cp $pkgdir/sapi/fpm/init.d.php-fpm    /etc/init.d/php-fpm
+	chmod a+x /etc/init.d/php-fpm	
+}
 
+function conf_nginx {
+	local conf=$1/conf/nginx.conf
+    local num=$( grep -n   "pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000" $conf | cut -d ":" -f 1   )
+    local start=$((num+2 ))
+    local  end=$((num+8))
+	echo $start
+	echo $end
+    sed -i  "$start,$end s/#//g" $conf &>/dev/null
+
+    let end=end-1
+    sed  -i "$end s/fastcgi_params/fastcgi.conf/g" $conf  &>/dev/null
+echo "<?php
+		phpinfo();
+?>
+" > $conf/html/index.php
+
+}
+
+function install_php {
+#  参数：
+#	$1:  软件包绝对路径
+#	$2： 运行软件的用户
+#	$3:	 编译配置
+   local pkg=$1
+   local user=$2
+
+yum -y install libxml2-devel libjpeg-devel libpng-devel freetype-devel curl-devel openssl-devel sqlite-devel oniguruma oniguruma-devel
+
+local configure=$3
+install_pkg $(tar_xf  $pkg $user ) $configure
+
+}
+
+function php_main {
+	local pkg=$PHP_pkg
+	local user=$PHP_user
+	local basedir=$PHP_basedir
+	local configure=$PHP_configure
+	install_php $pkg  $user  $configure
+	init_php $basedir  $PKG_DIR
+	conf_nginx $NGINX_basedir 
+}
+
+[ "$PHP_TAG" == "ON"  ] && php_main
+##################################################
